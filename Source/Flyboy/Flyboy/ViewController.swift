b761fde09dd75861de67dddd4a7dd144814c4c79
Colton <coltonboyd503@icloud.com>
2025-07-07 18:28:35 -0400

    Created default Xcode project for #4.

---
+++ Source/Flyboy/Flyboy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flyboy
+//
+//  Created by Colton Boyd on 7/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+  override func viewDidLoad()
+  {
+    super.viewDidLoad()
+    
+    self.view.backgroundColor = .red;
+  }
+}
+
