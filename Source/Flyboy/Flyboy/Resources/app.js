433f83829ee48b38c7fb7bc2791d99da9928b143
Colton <coltonboyd503@icloud.com>
2025-08-14 23:20:36 -0400

    Refactored and commented splash scene. Closes #98.

--- Source/Flyboy/Flyboy/Resources/app.js
+++ Source/Flyboy/Flyboy/Resources/app.js
@@ -2,43 +2,44 @@
 // SCENES
 ///////////////////////////////////////////////////////////
 
+/** Class representing the splash scene of Flyboy. */
 class SplashScene extends Phaser.Scene 
 {
+  /** Public constructor. */
   constructor() 
   {
     super('SplashScene');
   }
 
+  /** Public method called to pre-load any assets for the scene. */
   preload()
   {
     let font = new FontFace('BulgariaDreams', 'url("Bulgaria Dreams Regular.ttf")');
     font.load().then((loadedFace) => { document.fonts.add(loadedFace);})
     .catch((err) => { console.warn('Font failed to load', err); });
-    
-    this.load.audio('menu-music', 'menu-music.mp3');
-    this.load.image('logo', 'scriptit-logo.png')
 
-    if(app.isFirstLaunch === true ) 
+    this.load.audio('menu-music', 'menu-music.mp3');
+    this.load.image('logo', 'scriptit-logo.png');
+  }
+
+  /** Public method called to create logic and assets for the scene. */
+  create() 
+  {
+    this.cameras.main.setBackgroundColor('#F0DB4F');
+
+    let settings = saveData.getSettings();
+    if(settings.soundOn === true) this.sound.mute = false;
+    else this.sound.mute = true;
+
+    if(app.isFirstLaunch === true) 
     {
       saveData.addLevelProgress({ id: 1, stars: 0, unlocked: true, score: 0 });
       saveData.addSettings({ soundOn: true });
     }
 
-    let settings = saveData.getSettings();
-    if(settings.soundOn === true) this.sound.mute = false;
-    else this.sound.mute = true;
-  }
-
-  create() 
-  {
-    this.cameras.main.setBackgroundColor('#F0DB4F');
-
     let logo = this.add.image(this.scale.width / 2, this.scale.height / 2, 'logo');
+    logo.setScale(Math.min(this.scale.width * 0.4 / logo.width, 1));
     logo.setOrigin(0.5);
-
-    let maxLogoWidth = this.scale.width * 0.4;
-    let scaleFactor = Math.min(maxLogoWidth / logo.width, 1);
-    logo.setScale(scaleFactor);
 
     this.tweens.add({ targets: logo, alpha: 0, duration: 2000, delay: 1000, ease: 'Linear' });
     setTimeout(() => { this.scene.start('MainMenuScene'); }, 3000);
@@ -63,7 +64,6 @@ class LevelSelectScene extends Phaser.Scene
     this.load.image('star-gold', 'star-gold.png');
     this.load.image('star-silver', 'star-silver.png');
     this.load.image('lock', 'lock.png');
-
     this.load.image('back-button', 'back-button.png');
   }
 
