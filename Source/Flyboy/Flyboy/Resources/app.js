db5236602a05c1769a2c0fae3080d8bf32c38ca9
Colton <coltonboyd503@icloud.com>
2025-08-18 21:57:22 -0400

    Refactored and commented Joystick class. Closes #49.

--- Source/Flyboy/Flyboy/Resources/app.js
+++ Source/Flyboy/Flyboy/Resources/app.js
@@ -1231,85 +1231,59 @@ class Plane
 
 /////////////////////////////////////////////////
 
+/** Class representing the joystick in the HUD of the game scene. */
 class Joystick 
 {
   base;
   centerY;
   currentState;
   dragRange;
-  errors;
   scene;
+  states;
   stick;
 
+  /**
+   * Creates the joystick object. 
+   * @param {Phaser.Scene} scene - Scene instance.
+   */
   constructor({ scene } = {}) 
   {
-    this.errors = 
-    {
-      sceneError: 'Joystick Error: A valid phaser scene is required.'
-    };
+    this.states = { idle: 'idle', up: 'up', down: 'down' };
 
-    this.states = 
-    {
-      idle: 'idle',
-      up: 'up',
-      down: 'down'
-    }
-
-    if(!scene) 
-    {
-      console.error(this.errors.sceneError);
-      return;
-    }
+    if(!scene) console.error('Joystick Error: A valid phaser scene is required.');
 
     this.scene = scene;
     this.currentState = this.states.idle;
 
-    const targetHeight = device.screenWidth / 5;
     this.base = scene.add.image(0, 0, 'joystick-base');
-    const baseScale = targetHeight / this.base.height;
-    this.base.setScale(baseScale);
+    this.base.setScale((device.screenWidth / 5) / this.base.height);
 
     this.stick = scene.add.image(0, 0, 'joystick');
-    this.stick.setScale(baseScale);
+    this.stick.setScale((device.screenWidth / 5) / this.base.height);
     this.stick.setInteractive();
     this.scene.input.setDraggable(this.stick);
 
-    const padding = 20;
-    const baseDisplayWidth = this.base.displayWidth;
-    const baseDisplayHeight = this.base.displayHeight;
+    this.base.setPosition(20 + (this.base.displayWidth) / 2, device.screenWidth - 20 - (this.base.displayHeight) / 2);
+    this.stick.setPosition(20 + (this.base.displayWidth) / 2, device.screenWidth - 20 - (this.base.displayHeight) / 2);
 
-    const x = padding + baseDisplayWidth / 2;
-    const y = device.screenWidth - padding - baseDisplayHeight / 2;
-    this.base.setPosition(x, y);
-    this.stick.setPosition(x, y);
-
-    this.centerY = y;
-    this.dragRange = baseDisplayHeight / 4;
+    this.centerY = device.screenWidth - 20 - (this.base.displayHeight) / 2;
+    this.dragRange = this.base.displayHeight / 4;
 
     this.stick.on('drag', (pointer, dragX, dragY) => 
     {
-      const clampedY = Phaser.Math.Clamp(dragY, this.centerY - this.dragRange, this.centerY + this.dragRange);
-      this.stick.setPosition(x, clampedY);
-
-      const deltaY = clampedY - this.centerY;
-
+      let clampedY = Phaser.Math.Clamp(dragY, this.centerY - this.dragRange, this.centerY + this.dragRange);
+      this.stick.setPosition(20 + (this.base.displayWidth) / 2, clampedY);
+      let deltaY = clampedY - this.centerY;
       let newState = this.states.idle;
       if(deltaY < -10) newState = this.states.up;
       else if(deltaY > 10) newState = this.states.down;
-
-      if(newState !== this.currentState) 
-      {
-        this.currentState = newState;
-      }
+      if(newState !== this.currentState) this.currentState = newState;
     });
 
     this.stick.on('dragend', () => 
     {
-      this.stick.setPosition(x, this.centerY);
-      if(this.currentState !== this.states.idle) 
-      {
-        this.currentState = this.states.idle;
-      }
+      this.stick.setPosition(20 + (this.base.displayWidth) / 2, this.centerY);
+      if(this.currentState !== this.states.idle) this.currentState = this.states.idle;
     });
   }
 }
