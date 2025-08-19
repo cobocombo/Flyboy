367939054629bf6a69de5f9fdba2ced91a9ed62f
Colton <coltonboyd503@icloud.com>
2025-08-18 20:33:33 -0400

    Refactored and commented Pickup class. Closes #47.

--- Source/Flyboy/Flyboy/Resources/app.js
+++ Source/Flyboy/Flyboy/Resources/app.js
@@ -1451,6 +1451,7 @@ class Bullet
 
 /////////////////////////////////////////////////
 
+/** Class representing a pickup that can be spawned in the game scene. */
 class Pickup 
 {
   errors;
@@ -1460,35 +1461,81 @@ class Pickup
   soundEffect;
   speed;
   
+  /**
+   * Creates and spawns the pickup object. 
+   * @param {Phaser.Scene} scene - Scene instance.
+   * @param {object} data - Data object loaded from pickups.json.
+   * @param {string} type - Unique pickup type to be filtered from the data.
+   * @param {number} x - X-coordinate postion.
+   * @param {number} y - Y-coordinate postion.
+   */
   constructor({ scene, data, type, x, y }) 
   {
-    let pickupDef = data.pickups.find(p => p.name === type);
-    if(!pickupDef) console.error(`Pickup Error: No pickup definition found for type "${type}".`);
-   
+    this.errors = 
+    {
+      dataNotFoundError: 'Pickup Error: No data found for pickup.',
+      dataTypeError: 'Pickup Error: Expected type object for data.',
+      deltaTypeError: 'Pickup Error: Expected type number for delta.',
+      heightTypeError: 'Pickup Error: Expected type number for height.',
+      nameTypeError: 'Pickup Error: Expected type string for name.',
+      sceneError: 'Pickup Error: A valid phaser scene is required.',
+      scoreTypeError: 'Pickup Error: Expected type number for score.',
+      soundEffectTypeError: 'Pickup Error: Expected type object for soundEffect.',
+      speedTypeError: 'Pickup Error: Expected type number for speed.',
+      spriteTypeError: 'Pickup Error: Expected type string for sprite.',
+      typeTypeError: 'Pickup Error: Expected type string for type.',
+      xTypeError: 'Pickup Error: Expected type number for x.',
+      yTypeError: 'Pickup Error: Expected type number for y.'
+    };
+
+    if(!scene) console.error(this.errors.sceneError);
+    if(!typeChecker.check({ type: 'object', value: data })) console.error(this.errors.dataTypeError);
+    if(!typeChecker.check({ type: 'string', value: type })) console.error(this.errors.typeTypeError);
+    if(!typeChecker.check({ type: 'number', value: x })) console.error(this.errors.xTypeError);
+    if(!typeChecker.check({ type: 'number', value: y })) console.error(this.errors.yTypeError);
+
     this.scene = scene;
-    this.sprite = scene.add.sprite(x, y, pickupDef.name);
-    this.sprite.setScale((device.screenWidth / pickupDef.heightScale) / this.sprite.height);
+    let pickupData = data.pickups.find(p => p.name === type);
+    if(!pickupData) console.error(this.errors.dataNotFoundError);
 
-    this.name = pickupDef.name;
-    this.score = pickupDef.score;
-    this.soundEffect = pickupDef.soundEffect;
-
-    this.speed = device.screenWidth / pickupDef.speed;
+    if(!typeChecker.check({ type: 'string', value: pickupData.name })) console.error(this.errors.nameTypeError);
+    if(!typeChecker.check({ type: 'string', value: pickupData.sprite })) console.error(this.errors.spriteTypeError);
+    if(!typeChecker.check({ type: 'number', value: pickupData.height })) console.error(this.errors.heightTypeError);
+    if(!typeChecker.check({ type: 'number', value: pickupData.speed })) console.error(this.errors.speedTypeError);
+    if(!typeChecker.check({ type: 'number', value: pickupData.score })) console.error(this.errors.scoreTypeError);
+    if(!typeChecker.check({ type: 'object', value: pickupData.soundEffect })) console.error(this.errors.soundEffectTypeError);
+   
+    this.name = pickupData.name;
+    this.sprite = scene.add.sprite(x, y, this.name);
+    this.sprite.setScale((device.screenWidth / pickupData.height) / this.sprite.height);
+    this.speed = device.screenWidth / pickupData.speed;
+    this.score = pickupData.score;
+    this.soundEffect = pickupData.soundEffect;
   }
 
-  update({ delta } = {}) 
+  /** Public method to destroy the pickups sprite. */
+  destroy() 
   {
-    this.sprite.x -= (this.speed * delta) / 1000;
+    this.sprite.destroy();
   }
 
+  /** 
+   * Public method to return if the pickup is currently off screen or not.
+   * @returns {boolean} Returns true or false if the pickup is off screen or not. 
+   */
   isOffScreen() 
   {
     return this.sprite.x < -this.sprite.displayWidth;
   }
 
-  destroy() 
+  /**
+   * Public method to update the pickup in the main game scene update loop. 
+   * @param {number} delta - Time passed since last frame.
+   */
+  update({ delta } = {}) 
   {
-    this.sprite.destroy();
+    if(!typeChecker.check({ type: 'number', value: delta })) console.error(this.errors.deltaTypeError);
+    this.sprite.x -= (this.speed * delta) / 1000;
   }
 }
 
@@ -1559,7 +1606,7 @@ class Enemy
     if(!typeChecker.check({ type: 'array', value: enemyData.soundEffects })) console.error(this.errors.soundEffectsTypeError);
   
     this.name = enemyData.name;
-    this.sprite = scene.add.sprite(x, y, enemyData.name);
+    this.sprite = scene.add.sprite(x, y, this.name);
     this.sprite.setScale((device.screenWidth / enemyData.height) / this.sprite.height);
     this.sprite.play(enemyData.startingAnimation);
     this.sprite.setFlipX(enemyData.flipX);
